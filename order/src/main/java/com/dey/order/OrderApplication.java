79e56107e68258d54392f18f4db29c3c969c4670
DeyRan <DeyRan@gitdey.com>
2022-06-17 19:55:08 +0800

    分布式

---
+++ order/src/main/java/com/dey/order/OrderApplication.java
@@ -0,0 +1,25 @@
+package com.dey.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author deyran
+ * @date 2022/6/17
+ * @package com.dey.order.controller
+ */
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        RestTemplate restTemplate = builder.build();
+        return restTemplate;
+    }
+}
---
+++ stock/src/main/java/com/dey/stock/StockApplication.java
@@ -0,0 +1,16 @@
+package com.dey.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author deyran
+ * @date 2022/6/17
+ * @package com.dey.stock.controller.com.dey.stock
+ */
+@SpringBootApplication
+public class StockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockApplication.class, args);
+    }
+}
